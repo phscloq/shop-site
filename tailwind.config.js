16bbc0d336b1eb99dbbe557446f457f410d4af62
Baran Dogan <phscloq@gmail.com>
2023-07-24 00:17:24 +0300

    ADD custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        'paynes-gray': '#586f7c',
+        'g-red': '#c44900',
+      },
+      minHeight:{
+        '80': '780px',
+      },
+      flexGrow:{
+        3: '3',
+      },
+      flex:{
+        '2': '2 1 0%',
+        '6': '6 1 0%',
+      },
+      boxShadow:{
+        'newco': 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px',
+      },
     },
   },
   plugins: [],
