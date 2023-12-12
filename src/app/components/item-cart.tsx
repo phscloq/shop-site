3fc7912980b94409d04ffff15ee3a3e0b2abe822
Baran Dogan <phscloq@gmail.com>
2023-12-13 00:44:25 +0300

    Refactor item-cart component styling

--- src/app/components/item-cart.tsx
+++ src/app/components/item-cart.tsx
@@ -22,18 +22,35 @@ export default function ItemCart(){
       return(<>
         {filteredItemsArr.map((product:any)=>{
             return (
-                <div key={product.title} className=" bg-slate-50 flex flex-col h-[340px] border-2 rounded-lg">
-                    <div className="mx-auto p-2 h-44">
-                          <img src={product.image } className="h-full"></img>
+                <div key={product.title} 
+                className=" bg-slate-50 flex flex-col 
+                h-[340px] border-2 rounded-lg">
+                    {/*img div */}
+                    <div className="mx-auto p-2 h-46">
+                          <img src={product.image } className=" h-44"></img>
                     </div>
-                    <div className="px-8 pb-4 h-full border-0 border-t-2 flex flex-col justify-between">
-                        <div className="flex flex-col justify-between">
-                            <h3 className=" font-bold">{product.title}</h3>
-                            <p>Price: {product.price}</p>
+                    
+                    <div className="h-full 
+                    border-0 border-t-2 
+                     p-2
+                    flex flex-col justify-between">
+                        <div className="">
+                             <h3 className="text-xs ">{product.title}</h3>
                         </div>
-                       <button onClick={() => handleAddToCart(product)} className=" w-full p-2  bg-orange-500
-                    active:transition  active:scale-95  duration-500 hover:bg-orange-600  text-white">Add to cart</button>
-                </div>
+                        <div className="">                          
+                            <p className=" text-base font-bold">Price: {product.price}</p>
+                        </div>
+                        <div>
+                              <button onClick={() => handleAddToCart(product)} 
+                              className=" w-full p-2  bg-orange-500
+                          active:transition  active:scale-95  
+                          duration-500 hover:bg-orange-600
+                           rounded-lg  text-white">
+                            Add to cart
+                            </button>
+                        </div>
+                       
+                    </div>
                 </div>
             )
         })}
