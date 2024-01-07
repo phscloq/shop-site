ce61255c189e159660edfea0729c92cc5189cdac
Baran Dogan <phscloq@gmail.com>
2024-01-07 12:45:37 +0300

    Update checkout item card layout and styles

--- src/app/(tabs)/cart/components/c.out-item-card.tsx
+++ src/app/(tabs)/cart/components/c.out-item-card.tsx
@@ -6,21 +6,21 @@ export default function CheckOutItemCard() {
     
         {cartItems.map((item)=>(
             <div key={item.id} 
-            className="flex  shadow-newco rounded-xl p-6 mb-8 w-full">
+            className="flex justify-between  shadow-newco rounded-xl p-6 mb-8 w-full">
                 
                 <div className="">
-                    <img src={item.image} className="h-32 max-w-[116px]"></img>
+                    <img src={item.image} className="h-32 max-w-[110px]"></img>
                 </div>
 
-                <div className="flex justify-between items-center ">
+                <div className="flex flex-col justify-between sm:items-center max-w-[200px] ">
                     <div> {/* Item description */}
                         <div className=" max-w-md  pl-2">
-                            <h2 className="font-bold text-base">{item.title}</h2>
+                            <h2 className="font-bold text-sm truncate sm:text-base">{item.title}</h2>
                             <p className=" text-xs leading-6  truncate">{item.description}</p>
                         </div>
                     </div>
 
-                    <div className="flex gap-10">{/* Item's numbers */}
+                    <div className="flex  justify-between">{/* Item's numbers */}
                         <div className="flex h-9 border-2 items-center"> {/* Item quantity div */}
                                 <button className=" text-orange-500 text-lg px-2  border-0" 
                                         onClick={()=>addToCart(item)}>+</button>
@@ -31,7 +31,7 @@ export default function CheckOutItemCard() {
                         
                         </div>
 
-                        <div className="flex lg:items-center"> {/* Item price and delete div */}
+                        <div className="flex gap-2 items-center lg:items-center"> {/* Item price and delete div */}
                             <h3 
                             className="text-orange-500 text-xl  px-2 w-max">
                                 ${item.price}
