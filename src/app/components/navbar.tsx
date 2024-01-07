08fdbbe5605756ccd2056c62b1375549b0c85c8b
Baran Dogan <phscloq@gmail.com>
2024-01-07 12:24:23 +0300

    Refactor navbar component

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,58 +1,27 @@
-import Link from "next/link"
-import Image from "next/image";
+
 import OrderPreview from "./order-preview"
 import { useShoppingCart } from "../lib/shopppingCartContext";
+import SearchBar from "./components-nav/search-bar";
+import SubBar from "./components-nav/sub-bar";
+import Basket from "./components-nav/basket";
+import Logo from "./components-nav/logo";
 interface NavbarProps{
     activeLink:string;
 }
 
 export default function Navbar (){
-    const { cartItems, basket, handleCategoryClick, filterText, handleFilterTextChange} = useShoppingCart();
-    const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+    const {basket} = useShoppingCart();
     return (
-<nav className="w-full  pt-4  border-0 border-b-2 border-paynes-gray bg-orange-600     text-white">
-<div className="px-4 flex items-center justify-between gap-16">
-<Link href="./" onClick={()=>handleCategoryClick("home")}>
-     <div className="flex flex-col items-center">
-    <span className="text-4xl">Shop Allure</span>
-    </div></Link>
-
-{/*Search Bar */}
-<div className="grow shrink basis-auto w-auto text-black">
-    <input className=" rounded-md p-2 w-3/4"
-    value={filterText}
-    onChange={(e)=>handleFilterTextChange(e)}
-    placeholder="Search for the product"
-    ></input>
-</div>
-
-<div className="flex">
-{/*     <button className="rounded-md bg-orange-500 px-8">Sign-in</button>
- */}    <Link href="./cart">
-    <Image src="/svgs/shop-icon.svg" width={50} height={50} alt="Shopping basket icon" />
-    <div className="absolute top-12 bg-g-red rounded-full w-6 items-center flex justify-center">
-    <span className="  text-white">{totalItems}</span>
-    </div>
-   
-    </Link>
-
-
-</div>
-
-</div>
-<div className=" px-4 bg-orange-500 flex gap-4">
-    <Link href='./men' onClick={()=>handleCategoryClick("men's clothing")} className={`font-bold hover:underline `}>Men</Link>
-    <Link href='./women' onClick={()=>handleCategoryClick("women's clothing")} className="font-bold hover:underline">Women</Link>
-    <Link href='./electronics' onClick={()=>handleCategoryClick("electronics")} className="font-bold hover:underline">Electronics</Link>
-    <Link href='./jewelery' onClick={()=>handleCategoryClick("jewelery")} className="font-bold hover:underline">Jewelery</Link>
-
-
-
-</div>
-{basket ? <OrderPreview /> : null }
-
-
-</nav> 
+            <nav className="w-full  max-h-24 pt-4  border-0 border-b-2 border-paynes-gray
+                bg-orange-600  text-white">
+                <div className="px-4 flex items-center justify-between gap-16">
+                    <Logo />
+                    <SearchBar />
+                    <Basket />
+                </div>
+                <SubBar />
+                {basket ? <OrderPreview /> : null }
+            </nav> 
 
     )
 
