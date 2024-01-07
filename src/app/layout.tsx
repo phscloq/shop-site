c80da9422c05f478c5be9e3b9822e91d2045eb87
Baran Dogan <phscloq@gmail.com>
2024-01-07 12:18:08 +0300

    Update body class in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
    
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} lg:px-24`}>
       <Structure>
         {children}
       </Structure>
