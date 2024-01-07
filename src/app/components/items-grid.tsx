b1b6a4983a432525b2eba0b13732e0875c2a6b6a
Baran Dogan <phscloq@gmail.com>
2024-01-07 12:23:18 +0300

    Refactor item grid component

--- src/app/components/items-grid.tsx
+++ src/app/components/items-grid.tsx
@@ -1,9 +1,9 @@
-import ItemCart from "./item-cart"
+import ItemCard from "./item-card"
 
 export default function ItemsGrid(){
     return (
-        <div className="mt-4 flex-1 grid  grid-cols-4 gap-8 text-black">
-            <ItemCart />
+        <div className="mt-4 flex-1 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8 text-black">
+            <ItemCard />
         </div>
     )
 }
