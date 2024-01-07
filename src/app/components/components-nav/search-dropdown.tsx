ffc179f4b77894231e8ac32e6f68430023094c04
Baran Dogan <phscloq@gmail.com>
2024-01-07 22:51:35 +0300

    Add SearchDropDown component

---
+++ src/app/components/components-nav/search-dropdown.tsx
@@ -0,0 +1,26 @@
+import { useShoppingCart } from "@/app/lib/context";
+
+export default function SearchDropDown(){
+const {filteredItems, searchBarActive} = useShoppingCart();
+    return(
+        <div className={`${searchBarActive ? 'block': 'hidden'} min-h-[240px] max-h-80 
+        w-full  z-50 sm:w-3/4 
+        overflow-auto absolute bg-slate-100  shadow-2xl sm:border border-orange-500`}>
+           <div className="px-4 py-2">
+           <ul className="">
+                {filteredItems.map((item:any)=>{
+                    return(
+                        <li key={item.title} 
+                        className=" border-b border-slate-400 truncate p-2 hover:bg-slate-200 hover:text-black">
+                            {item.title}
+                        </li>   
+                      
+                      
+                    )
+                })}
+               
+            </ul>
+           </div>
+        </div>
+    )
+}
