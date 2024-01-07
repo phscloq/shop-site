43671bd811a10cd36da018481a73d0f05b1db92b
Baran Dogan <phscloq@gmail.com>
2024-01-07 12:22:02 +0300

    Refactor footer component

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link"
 export default function Footer(){
     return (
-        <footer className=" py-2 text-lg  text-black">
-        <p className="text-center">Made by 
-        <Link className=" ml-2 underline text-orange-500 font-bold" href={'https://github.com/phscloq'} target="_blank">Baran</Link>
-        </p>
+        <footer className=" py-2 text-lg  max-h-12 text-black">
+            <p className="text-center">Made by 
+                <Link className=" ml-2 underline text-orange-500 font-bold" href={'https://github.com/phscloq'} target="_blank">Baran</Link>
+            </p>
         </footer>
        
     )
