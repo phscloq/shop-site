fa4b02cad5f2ad94b1aef28316b458258d12d1d6
Baran Dogan <phscloq@gmail.com>
2024-01-07 22:52:32 +0300

    Refactor search bar and basket components

--- src/app/components/components-nav/basket.tsx
+++ src/app/components/components-nav/basket.tsx
@@ -1,19 +1,24 @@
 import Link from "next/link"
 import Image from "next/image"
-import { useShoppingCart } from "../../lib/shopppingCartContext";
+import { useShoppingCart } from "../../lib/context";
 export default function Basket(){
-    const { cartItems} = useShoppingCart();
+    const { cartItems, handleSearchBarActive} = useShoppingCart();
     const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
     return (
         <div className="flex gap-4">
-            <Image className=" text-white"
-             src="/svgs/search-icon.svg" width={45} height={45} alt="Search icon" />
+            <button
+            onClick={()=>handleSearchBarActive(true)}
+            >
+                <Image className=" sm:hidden text-white"
+                src="/svgs/search-icon.svg" width={45} height={45} alt="Search icon" />
+            </button>
+            
             <Link 
             className="relative"
             href="./cart">
                 <Image src="/svgs/shop-icon.svg" width={50} height={50} alt="Shopping basket icon" />
                 <div 
-                className="absolute top-8  bg-g-red rounded-full 
+                className="absolute top-8  z-40 bg-g-red rounded-full 
                 w-6 h-6  items-center flex justify-center">
                     <span 
                     className=" text-sm text-white">
--- src/app/components/components-nav/search-bar.tsx
+++ src/app/components/components-nav/search-bar.tsx
@@ -1,15 +1,28 @@
-import { useShoppingCart } from "../../lib/shopppingCartContext";
+import { useShoppingCart } from "../../lib/context";
+import SearchDropDown from "./search-dropdown";
 
 export default function SearchBar(){
-    const { filterText, handleFilterTextChange} = useShoppingCart();
+    const { filterText, handleFilterTextChange, handleSearchBarActive, searchBarActive} = useShoppingCart();
     return (
-        <div className="hidden sm:block grow shrink basis-auto w-auto text-black">
-            
-            <input className=" rounded-md p-2 w-3/4"
-            value={filterText}
-            onChange={(e)=>handleFilterTextChange(e)}
-            placeholder="Search for the product"
-            ></input>
+        <button onClick={()=>console.log("clicked")}>
+        <div className={`${searchBarActive  ? 'h-full fixed overflow-y-hidden w-screen top-0 left-0 backdrop-blur z-50': ''}
+        `}>
+            <div className={`${searchBarActive ? 'absolute left-0 top-0 w-screen z-50 block':'hidden sm:block'}
+            sm:block sm:relative sm:grow sm:shrink sm:basis-auto sm:w-auto text-black
+            `}>
+                
+                <input className="w-full text-xl py-6 px-4
+                sm:active:rounded-t-lg
+                sm:rounded-md sm:p-2 sm:w-3/4 sm:active:border sm:active:border-orange-500"
+                value={filterText}
+                onChange={(e)=>handleFilterTextChange(e)}
+                onFocus={()=>handleSearchBarActive(true)}
+                onBlur={()=>handleSearchBarActive(false)}
+                placeholder="Search for the product"
+                ></input>
+                <SearchDropDown />
+            </div>
         </div>
+        </button>
     )
 }
