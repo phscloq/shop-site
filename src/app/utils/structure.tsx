558d8acc94e5918637bcd23fc1afceb5a4e7e4d1
Baran Dogan <phscloq@gmail.com>
2024-01-09 19:55:11 +0300

    Refactor structure.tsx to use new context and add useEffect hook

--- src/app/utils/structure.tsx
+++ src/app/utils/structure.tsx
@@ -1,22 +1,31 @@
 'use client'
-
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import { ShoppingCartProvider } from "../lib/shopppingCartContext";
+import { ShoppingCartProvider, useShoppingCart } from "../lib/context";
+import { useEffect } from "react";
+
 interface StructureProps {
-    children: React.ReactNode;
-  }
+  children: React.ReactNode;
+}
 
-export default function Structure({children}:StructureProps){
+export default function Structure({ children }: StructureProps) {
+  const { searchBarActive } = useShoppingCart();
 
+  useEffect(() => {
+    console.log("searchBarActive updated:", searchBarActive);
+  }, [searchBarActive]);
 
-    return (
+  return (
     <ShoppingCartProvider>
-        <Navbar />
-        <div className="px-4  md:px-8 lg:px-0">
-            {children}
-        </div>
-        <Footer />
+      <Navbar />
+      <div
+        className={`${
+          searchBarActive ? 'overflow-y-hidden h-500' : 'overflow-y-auto'
+        } static px-4 md:px-8 lg:px-0`}
+      >
+        {children}
+      </div>
+      <Footer />
     </ShoppingCartProvider>
-    )
+  );
 }
