8321bf7ffa9585550af32fca8ea08c1432442ced
Baran Dogan <phscloq@gmail.com>
2024-01-14 22:59:18 +0300

    Refactor layout and structure components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import Structure from './utils/structure'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Shop Allure: Your One-Stop Online Shopping Destination',
@@ -22,12 +20,11 @@ export default function RootLayout({
   return (
    
     <html lang="en">
-      <body className={`${inter.className} lg:px-24`}>
+      
       <Structure>
         {children}
       </Structure>
       
-        </body>
     </html>
 
   )
--- src/app/utils/structure.tsx
+++ src/app/utils/structure.tsx
@@ -1,33 +1,34 @@
 'use client'
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import { ShoppingCartProvider, useShoppingCart } from "../lib/context";
-import { useEffect } from "react";
+import { ShoppingCartContext, ShoppingCartProvider} from "../lib/context";
 import { SearchBarMobile } from "../components/components-nav/search-bar";
+import DarkOverlay from "../components/dark-overlay";
+import { Inter } from 'next/font/google'
+import { useContext } from "react";
 
+const inter = Inter({ subsets: ['latin'] })
 interface StructureProps {
   children: React.ReactNode;
 }
 
 export default function Structure({ children }: StructureProps) {
-  const { searchBarActive } = useShoppingCart();
-
-  useEffect(() => {
-    console.log("searchBarActive updated:", searchBarActive);
-  }, [searchBarActive]);
-
+const {mobileSearchBarActive} = useContext(ShoppingCartContext);
+  
   return (
     <ShoppingCartProvider>
+              
+      <body className={`${inter.className} ${mobileSearchBarActive ? 'overflow-hidden':''} lg:px-24 `}>
+      <DarkOverlay />
       <Navbar />
       <SearchBarMobile />
       <div
-        className={`${
-          searchBarActive ? 'overflow-y-hidden h-500' : 'overflow-y-auto'
-        } static px-4 md:px-8 lg:px-0`}
+        className={` aria-hidden:${mobileSearchBarActive}  ${mobileSearchBarActive ? ' pointer-events-none': ' '}  px-4 md:px-8 lg:px-0`}
       >
         {children}
       </div>
       <Footer />
+      </body>
     </ShoppingCartProvider>
   );
 }
