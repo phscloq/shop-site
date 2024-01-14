4ab880563d0a60cabcc6d584ace7093a2eb0dbee
Baran Dogan <phscloq@gmail.com>
2024-01-14 22:59:46 +0300

    Refactor code to remove unnecessary filtering and add mobile search bar functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,10 @@
 import { useShoppingCart } from "./lib/context"
 import ItemsGrid from "./components/items-grid"
 export default function Home() {
-  const {allItems} = useShoppingCart();
-const menItems=allItems.filter((item)=>item.category==="men's clothing");
-const womenItems=allItems.filter((item)=>item.category==="women's clothing");
-const jewelery=allItems.filter((item)=>item.category==="jewelery");
-const electronics=allItems.filter((item)=>item.category==="electronics");
+  const {mobileSearchBarActive} = useShoppingCart();
 
-  return (    <main className="mt-8 ">
+  return (    <main className={`mt-8 ${mobileSearchBarActive ? ' pointer-events-none': ' '}
+  `}>
      <ItemsGrid />
 
        
