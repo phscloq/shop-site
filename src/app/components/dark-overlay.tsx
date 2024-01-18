7faca421406d1f4b94489af644736e2d55d50e7e
Baran Dogan <phscloq@gmail.com>
2024-01-18 14:31:54 +0300

    Add overflow control for mobile search bar and extended cart

--- src/app/components/dark-overlay.tsx
+++ src/app/components/dark-overlay.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import { useShoppingCart } from "../lib/context"
 export default function DarkOverlay(){
-    const {mobileSearchBarActive} = useShoppingCart();
+    const {mobileSearchBarActive, extended} = useShoppingCart();
+    useEffect(()=>{
+        if(mobileSearchBarActive || extended){
+            document.body.style.overflowY = 'hidden';
+        }else{
+            document.body.style.overflowY = 'auto';
+        }
+    })
+
+
     return(
-            <div className={`${mobileSearchBarActive ? 'fixed top-0 left-0 overflow-y-hidden  w-full h-screen bg-black bg-opacity-50  z-40':' hidden'}
+            <div className={`${mobileSearchBarActive || extended ? 'fixed top-0 left-0 overflow-y-hidden  w-full h-screen bg-black bg-opacity-50  z-30':' hidden'}
                     
                     `}>
                     </div>
