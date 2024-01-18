1c7c5822f727491384f489954b345ed5794e6612
Baran Dogan <phscloq@gmail.com>
2024-01-18 23:52:52 +0300

    Add handleCategoryClick to Women page

--- src/app/products/women/page.tsx
+++ src/app/products/women/page.tsx
@@ -5,11 +5,12 @@ import { useShoppingCart } from "@/app/lib/context"
 
 
 export default function Women(){
-const {category} = useShoppingCart();
+const {category, handleCategoryClick} = useShoppingCart();
 
     return(
         <main className="flex max-w-screen-2xl  mx-auto gap-12 mt-8 ">
           <LeftNavBar
+            onClick={handleCategoryClick}
           tab={"women"}
           activeCategory={category}
           />
