05f5d9f6ca908b9db6fbe99b33ec299b6512c370
Baran <phscloq@gmail.com>
2024-12-10 23:22:17 +0300

    Update interface.sts
    Added quantity to `Product`.

--- src/app/lib/interfaces.ts
+++ src/app/lib/interfaces.ts
@@ -5,6 +5,7 @@ export interface Product {
     description: string;
     category: string;
     image: string;
+    quantity: number;
   }
  export  interface Cart extends Product {
     quantity: number;
