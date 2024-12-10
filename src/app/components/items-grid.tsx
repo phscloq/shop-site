a1852dfe54c6936f335baea5ca04c292b957b1e8
Baran <phscloq@gmail.com>
2024-12-10 23:12:24 +0300

    Update items-grid.tsx
    Added category prop to fetchItems based on the category dynamically.

--- src/app/components/items-grid.tsx
+++ src/app/components/items-grid.tsx
@@ -1,9 +1,25 @@
+'use client'
+import { useEffect, useState } from "react";
+import { useShoppingCart } from "../lib/context";
 import ItemCard from "./item-card"
+import { Product } from "../lib/interfaces";
+import { GetSpecificCategory } from "../data/GetSpecificCategory";
 
-export default function ItemsGrid(){
+export default function ItemsGrid({category}:{category?:string}){
+    const {allItems} = useShoppingCart();
+    const [items, setItems] = useState<Product[]>([]);
+    useEffect(() => {
+        const fetchItems = async () => {
+            const items = await GetSpecificCategory({category});
+            setItems(items);
+            /* setItems(allItems.filter((item:Product)=>item.category.toLowerCase().replace(/ /g, '-') === category)); */
+        };
+        fetchItems();
+    }, [category]);
+
     return (
-        <div className="mt-4 flex-1 grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-8 text-black">
-            <ItemCard />
+        <div className="mt-4 flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 text-black">
+            <ItemCard items={items}/>
         </div>
     )
 }
