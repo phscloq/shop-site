1bcfeecf0e52176828d07abe4a662bd614858ef4
Baran <phscloq@gmail.com>
2024-12-10 23:28:14 +0300

    Update logo.tsx
    Updated to comment out handleCategoryClick function and onClick.

--- src/app/components/navbar-components/logo.tsx
+++ src/app/components/navbar-components/logo.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link"
 import { useShoppingCart } from "../../lib/context";
 export default function Logo(){
-    const {handleCategoryClick} = useShoppingCart();
-    return (
+/*     const {handleCategoryClick} = useShoppingCart();
+ */    return (
         <Link className="h-full" href="/" 
-        onClick={()=>handleCategoryClick('')}
+       /*  onClick={()=>handleCategoryClick('')} */
         >
                 <div className="flex flex-col items-center w-44  h-full  lg:w-54 xl:w-auto">
                     <span className="text-3xl lg:text-3xl xl:text-4xl">
