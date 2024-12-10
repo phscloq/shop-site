232a50d294aca4a272d7c78d4b4a4ca20808495f
Baran <phscloq@gmail.com>
2024-12-10 23:05:53 +0300

    Update GetData function and file names to GetProducts

--- src/app/data/GetProducts.ts
+++ src/app/data/GetProducts.ts
@@ -1,7 +1,8 @@
-export async function GetData(){
+export async function GetProducts(){
         
           const response = await fetch('https://fakestoreapi.com/products/');
           const data = await response.json();
           return data;
-      
 }
+// https://fakestoreapi.com/products/ 
+// https://api.escuelajs.co/api/v1/products
