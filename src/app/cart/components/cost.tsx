7d4158cba132eac7f0e6020441f2f06c41b3c084
Baran <phscloq@gmail.com>
2024-12-11 21:09:37 +0300

    Update cost.tsx
    Updated styling.

--- src/app/cart/components/cost.tsx
+++ src/app/cart/components/cost.tsx
@@ -8,17 +8,17 @@ export default function Cost(){
         <div className={` sm:col-start-2 `}>
             <div className={`text-black sm:sticky  sm:top-8 border-2`}>
                 <div className="">
-                    <h1 className=" p-2 bg-orange-500 text-white font-bold">Summary</h1>
+                    <h1 className=" p-2 bg-blue-500 text-white font-bold">Summary</h1>
                     <div className="p-2 mb-4 leading-8">
                         <p>Total cost:</p>
-                        <p className=" text-orange-500 font-bold">${cost}</p>
+                        <p className=" text-blue-500 font-bold">${cost}</p>
                     </div>
                 </div>
                 <div className="p-2">
                     <button 
-                    className=" w-full p-2  bg-orange-500
+                    className=" w-full p-2  bg-blue-500
                     active:transition  active:scale-95  
-                    duration-500 hover:bg-orange-600
+                    duration-500 hover:bg-blue-600
                     rounded-lg  text-white">
                         Purchase
                     </button>
