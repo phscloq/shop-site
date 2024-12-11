a8d53dd46290e098f819c5575d086579b344cb80
Baran <phscloq@gmail.com>
2024-12-11 21:09:51 +0300

    Update footer.tsx
    Updated styling.

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer(){
         <footer className="  bg-gray-100">
             <div className="container mx-auto px-4 py-8">
                 <p className="text-center text-gray-600">Made by 
-                    <Link className=" ml-2 text-orange-500 font-bold" href={'https://github.com/phscloq'} target="_blank">Baran</Link>
+                    <Link className=" ml-2 text-blue-500 font-bold" href={'https://github.com/phscloq'} target="_blank">Baran</Link>
                 </p>
             </div>
         </footer>
