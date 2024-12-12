e2fac80867c1bf7b612083d880cd62c6cd9f273d
Baran <phscloq@gmail.com>
2024-12-12 10:53:43 +0300

    Update homepage
    Added `ProductSlider` to the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 import { useShoppingCart } from "./lib/context"
 import ItemsGrid from "./components/items-grid"
+import ProductSlider from "./components/product-slider";
 export default function Home() {
   const {mobileSearchBarActive} = useShoppingCart();
 
-  return (    <main className={`mt-8 ${mobileSearchBarActive ? ' pointer-events-none': ' '}
+  return (    
+  <main className={`mt-8 ${mobileSearchBarActive ? ' pointer-events-none': ' '}
   `}>
-     <ItemsGrid />
-
-       
+    <ProductSlider />
+    <ItemsGrid />      
       
     </main>
   )
