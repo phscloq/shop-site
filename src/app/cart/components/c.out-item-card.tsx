e197be4f252362a1dd21e24b05d524183d8cfcd9
Baran <phscloq@gmail.com>
2024-12-12 10:50:03 +0300

    Update checkout item card styling

--- src/app/cart/components/c.out-item-card.tsx
+++ src/app/cart/components/c.out-item-card.tsx
@@ -2,53 +2,53 @@ import { useShoppingCart } from "@/app/lib/context";
 
 export default function CheckOutItemCard() {
     const {cartItems, addToCart, deleteItem} = useShoppingCart();
-    return (<>
+    return (<div className="w-full lg:w-3/4">
     
-        {cartItems.map((item)=>(
-            <div key={item.id} 
-            className="flex justify-between border-b p-6 mb-8 w-full ">
-                
-                <div className="">
-                    <img src={item.image} alt={item.title} className="h-32 "></img>
+    {cartItems.map((item)=>(
+        <div key={item.id} 
+            className="flex  lg:flex-row border-b p-4 mb-4 w-full ">
+                <div className="w-1/4 mb-4 lg:mb-0">
+                    <img src={item.image} alt={item.title} className="h-32  "></img>
                 </div>
 
-                <div className="flex flex-col justify-between   ">
-                    <div> {/* Item description */}
-                        <div className=" max-w-md  pl-2">
-                            <h2 className="font-bold text-sm truncate sm:text-base">{item.title}</h2>
-                            <p className=" text-xs leading-6  truncate">{item.description}</p>
-                        </div>
+                <div className="flex flex-col justify-between w-3/4 pl-4">
+                        
+                    <div className="mb-2 lg:mb-0">
+                        <h2 className="font-bold text-sm lg:text-base mb-1">{item.title}</h2>
+                        {/* <p className=" text-xs leading-5 line-clamp-2">{item.description}</p> */}
                     </div>
 
-                    <div className="flex  self-end space-x-4 ">{/* Item's numbers */}
+                    <div className="flex  justify-between items-center mt-2 lg:mt-0">{/* Item's numbers */}
+                        
                         <div className="flex h-9 border-2 items-center"> {/* Item quantity div */}
                                 <button className=" text-orange-500 text-lg px-2  border-0" 
-                                        onClick={()=>addToCart(item)}>+</button>
-                                        <p className=" border-2 px-2 border-y-0  text-sm w-9 text-center">{item.quantity}</p>
-                                        <button className={`text-lg px-2 
-                                        border-0 ${item.quantity==1 ? ' text-gray-400 pointer-events-none' : 'text-orange-500'}`} 
-                                        onClick={()=>deleteItem(item)}>-</button>
-                        
+                                        onClick={()=>addToCart(item)}
+                                    >
+                                        +
+                                </button>
+                                <p className=" border-2 px-2 border-y-0  text-sm w-9 text-center">
+                                    {item.quantity}
+                                </p>
+                                <button className={`text-lg px-2 
+                                border-0 ${item.quantity==1 ? ' text-gray-400 pointer-events-none' : 'text-orange-500'}`} 
+                                onClick={()=>deleteItem(item)}
+                                >
+                                    -
+                                </button>
                         </div>
 
-                        <div className="flex gap-2 items-center lg:items-center"> {/* Item price and delete div */}
-                            <h3 
-                            className="text-orange-500 text-xl  px-2 w-max">
+                        <h3 className="text-orange-500 text-xl  px-2">
                                 ${item.price}
-                            </h3>
-                            <button 
-                            className={` text-red-600 font-bold`}
-                            onClick={()=>deleteItem(item)}>
-                                    X
-                            </button>
-                        </div>
+                        </h3>
+                        
                     </div>
+
                 </div>
            
         </div>
 
         ))}
     
-    </>)
+    </div>)
    
 }
