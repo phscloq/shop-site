4485ef3a9e2abf60a946336b4d2cacf6a12eb9ce
Baran <phscloq@gmail.com>
2024-12-12 10:50:55 +0300

    Update navbar.tsx
    Updated styling and deleted unused imports.

--- src/app/components/header-components/navbar.tsx
+++ src/app/components/header-components/navbar.tsx
@@ -1,15 +1,13 @@
 'use client'
 import Link from "next/link"
 import { useShoppingCart } from "../../lib/context";
-import { usePathname } from "next/navigation";
 export default function Navbar(){
 const {/* handleCategoryClick,  */selectedCategory} = useShoppingCart();
-const pathname = usePathname();
 const categories = ["men's clothing", "women's clothing", "jewelery", "electronics"];
 
 
 return (
-<nav className=" flex  gap-4 ">
+<nav className=" flex flex-wrap gap-4 ">
    {categories.map((category:string, index:number)=>{
          return (
               <Link key={index} href={`/category/${category.toLowerCase().replace(/ /g, '-')}`} 
