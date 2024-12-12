5276c4820b91e1316e3c45a85d0eb5b74b6101f9
Baran <phscloq@gmail.com>
2024-12-12 22:13:09 +0300

    Update cart.tsx
    Removed svg and added `ShoppingCart` from lucide-react.

--- src/app/components/header-components/cart.tsx
+++ src/app/components/header-components/cart.tsx
@@ -1,16 +1,19 @@
+'use client'
 import Link from "next/link"
 import Image from "next/image"
 import { useShoppingCart } from "../../lib/context";
 import OrderPreview from "../order-preview";
+import { ShoppingCart } from "lucide-react";
 export default function Cart(){
     const { cartItems, basket} = useShoppingCart();
     const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+
     return (
         <div className="relative">
             <Link 
             className=""
             href="/cart">
-                <Image src="/svgs/shop-icon.svg" width={24} height={24} alt="Shopping basket icon" />
+                <ShoppingCart className="w-6 h-6 text-black" />
                 {totalItems > 0 && (
                     <span 
                     className=" absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
