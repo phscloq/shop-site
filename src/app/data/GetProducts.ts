01269212bf286f509af302168fb8078cd0237f9d
Baran <phscloq@gmail.com>
2024-12-12 22:17:45 +0300

    Update GetProducts.ts
    Added map method to add default value of quantity to the products.

--- src/app/data/GetProducts.ts
+++ src/app/data/GetProducts.ts
@@ -1,8 +1,12 @@
+import { Product } from "../lib/interfaces";
 export async function GetProducts(){
         
           const response = await fetch('https://fakestoreapi.com/products/');
           const data = await response.json();
-          return data;
+          return data.map((product: Product) => ({
+            ...product,
+            quantity: product.quantity ?? 1, // Default to 1 if not already defined
+          })) as Product[];
 }
 // https://fakestoreapi.com/products/ 
 // https://api.escuelajs.co/api/v1/products
@@ -12,5 +16,8 @@ export async function GetLimitedProducts(limit: number){
         
   const response = await fetch(`https://fakestoreapi.com/products?limit=${limit}`);
   const data = await response.json();
-  return data;
+  return data.map((product: Product) => ({
+            ...product,
+            quantity: product.quantity ?? 1, // Default to 1 if not already defined
+          })) as Product[];
 }
