e5bbbe3af31458826de6ac499f652f231ce5cb95
Baran <phscloq@gmail.com>
2024-12-12 10:50:32 +0300

    Update cart page
    Updated styling.

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -7,16 +7,17 @@ export default function Cart(){
 const {cartItems} = useShoppingCart();
 const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
 return (
-    <main className="flex flex-col gap-12 mt-8 
-    sm:min-h-[calc(100vh-160px)]  sm:grid sm:grid-cols-[600px_160px] lg:grid-cols-[1fr_240px]  lg:mx-auto ">
-        <div className={ `text-black  min-h-[50vh]   sm:col-start-1 sm:col-end-2`}>
-            <h1 className=" text-2xl mb-8">My basket   ({totalItems} Items)</h1>   
+    <div className="text-black ">
+        
+        <h1 className=" text-2xl mb-8">My basket   ({totalItems} Items)</h1>   
+        <div className="flex flex-col lg:flex-row">
             <CheckOutItemCard />
+            <Cost />
         </div>
-        <Cost />
+        
         
          
-</main>
+    </div>
    
 )
 
