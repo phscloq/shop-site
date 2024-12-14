19578a76a7db923e1268bd2c9f599cf1046abd48
Baran <phscloq@gmail.com>
2024-12-14 09:50:51 +0300

    Update Home page
    Removed unused ShoppingCart context and updated the styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-'use client'
-import { useShoppingCart } from "./lib/context"
+
 import ItemsGrid from "./components/items-grid"
 import ProductSlider from "./components/product-slider";
 export default function Home() {
-  const {mobileSearchBarActive} = useShoppingCart();
 
   return (    
-  <main className={`mt-8 ${mobileSearchBarActive ? ' pointer-events-none': ' '}
-  `}>
+  <main className="container mx-auto px-4 py-8 text-black">
     <ProductSlider />
     <ItemsGrid />      
       
